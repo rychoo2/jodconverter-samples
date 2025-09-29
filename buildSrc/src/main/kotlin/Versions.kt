511658fcf8d8ba94a82802104ee12f35c02a5196
Juliusz Skowron <juliusz.skowron@draftable.com>
2025-09-29 12:44:58 +0200

    Update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
 
     //  Latest Version:
     //  Visit https://central.sonatype.com and search for: g:org.springframework.boot a:spring-boot
-    const val springBoot = "3.4.1"
+    const val springBoot = "3.4.9"
 
     object Plugins {
 
@@ -18,15 +18,15 @@ object Versions {
 
         //  Latest Version:
         //  Visit https://central.sonatype.com and search for: g:org.jodconverter a:jodconverter-core
-        const val jodConverter = "4.4.7"
+        const val jodConverter = "4.4.11"
 
         //  Latest Version:
         //  Visit https://central.sonatype.com and search for: g:commons-fileupload a:commons-fileupload
-        const val commonsFileUpload = "1.5"
+        const val commonsFileUpload = "1.6"
 
         //  Latest Version:
         //  Visit https://central.sonatype.com and search for: g:commons-io a:commons-io
-        const val commonsIo = "2.15.1"
+        const val commonsIo = "2.20.0"
 
         //  Latest Version:
         //  Visit https://central.sonatype.com and search for: g:org.springdoc a:springdoc-openapi-starter-webmvc-ui
@@ -42,11 +42,9 @@ object Versions {
 
         //  Latest Version:
         //  Visit https://central.sonatype.com and search for: g:io.swagger.core.v3 a:swagger-project
-        const val swagger = "2.2.20"
+        const val swagger = "2.2.38"
 
         // https://search.maven.org/artifact/org.apache.tomcat.embed/tomcat-embed-core
-        // We can't use 9.0.31+ until this is resolved:
-        // https://github.com/bmuschko/gradle-tomcat-plugin/pull/209
-        const val tomcat = "9.0.30"
+        const val tomcat = "9.0.108"
     }
 }
